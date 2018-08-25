4bd5e1ee574302703813b82f05347447a15e6317
IbnBase <sacko202@gmail.com>
2018-08-25 08:51:11 +0000

    creation de pages via controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :blogs
-  resources :guides
-   root to: 'blogs#index'
+  root to: 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
