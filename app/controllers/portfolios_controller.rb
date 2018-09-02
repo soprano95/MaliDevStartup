eedb63908c2b58bc4f03913bf8f9b6aab7c04e2f
IbnBase <sacko202@gmail.com>
2018-09-02 18:48:16 +0000

    Implematation partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -29,6 +29,7 @@ class PortfoliosController < ApplicationController
 
   def edit
   	@portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
    def update
