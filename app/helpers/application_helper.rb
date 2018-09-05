c2cccf3345268cdf024715985bba4f2f6a2b16fd
IbnBase <sacko202@gmail.com>
2018-09-05 22:31:57 +0000

    Implemented authorization engine for portfolio items and blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,21 @@
 module ApplicationHelper
 	def login_helper 
-	    if current_user.is_a?(User)
-	    	link_to "Logout", destroy_user_session_path, method: :delete
-	      else
-	     
-	      (link_to "Sign_Up", new_user_registration_path)+
-	      "<br>".html_safe +
-	       (link_to "Login", new_user_session_path)
-        end
+	   if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete
     end
-
+  end
+    
     def source_helper(layout_name)
     	if session[:source]
     		greeting = "Thanks for visiting me me from #{session[:source]} and you are on the #{layout_name} layout"
     		content_tag(:p, greeting, class: "source_greeting")
     	end
+    end
+    def copyright_generator
+    	MaliDevStartupViewTool::Renderer.copyright 'Abdoul Aziz Sacko', 'All rights reserved'
     end
 end
