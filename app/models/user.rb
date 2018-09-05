4de0c2c955f19437ecb5c8a363addfda363476f1
IbnBase <sacko202@gmail.com>
2018-09-05 15:53:46 +0000

    Implemented Error Management in rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,19 @@ class User < ApplicationRecord
          validates_presence_of :name
 
          def first_name
-         	self.name.split.first
+           	self.name.split.first.oops
          end
 
          def last_name
          	self.name.split.last
+         end
+
+         def get_facebook_messages
+          begin
+           contact_fb
+           @messages = retrieves_messages
+          rescue IOError => e
+            flash[:error] = "Error occurred contacting Facebook: #{e}"
+          end
          end
 end
