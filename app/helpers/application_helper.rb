e65f203ff772822d17f73e76fd315a87a10484d6
IbnBase <sacko202@gmail.com>
2018-10-04 07:09:04 +0000

    Réactivation des alertes dans la méthode helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,16 @@ module ApplicationHelper
     def copyright_generator
     	MaliDevStartupViewTool::Renderer.copyright 'Abdoul Aziz Sacko', 'Tous les droits sont réservés'
     end
+
+    def alerts
+      alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+      if alert
+        alert_generator alert
+      end
+    end
+
+    def alert_generator msg
+      js add_gritter(msg, title: " Aziz Portefeuille", sticky: false)
+    end
 end
