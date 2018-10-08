34815cca3b6c977699cbbe3604f4a13aa1539f82
IbnBase <sacko202@gmail.com>
2018-10-08 01:34:15 +0000

    Implemented jquery function for action cable comment feature

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,8 +14,15 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    if logged_in?(:site_admin) || @blog.published?
+      @blog = Blog.includes(:comments).friendly.find(params[:id])
+      @comment = Comment.new
+    
     @page_title = @blog.title
     @seo_keywords = @blog.body
+    else
+      redirect_to blogs_path, notice: "Vous n'êtes pas autorisé à accéder à cette page"
+    end
   end
 
   # GET /blogs/new
